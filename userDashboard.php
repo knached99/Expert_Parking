94f5347d99f8c18e947862417f5f2fb93df8bed0
Khaled Nached <khalednached@Khaleds-MacBook-Pro.local>
2020-09-17 09:35:27 -0400

    created dashboard file

---
+++ userDashboard.php
@@ -0,0 +1,24 @@
+<?php
+include('sendToDb.php');
+?>
+<!DOCTYPE HTML>
+<html lang = "en">
+<head>
+<meta charset = "utf-8">
+<meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+<link rel = 'stylesheet' type="text/css" href='dashBoard.css'>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<?php
+if(isset($_SESSION['userId']))
+{
+  echo '<p class="loginStatus">Hi ""'.$name'"", welcome to your dashboard</p>';
+
+}
+else{
+  echo '<p class="loginStatus">You are now logged out</p>';
+}
+?>
+</body>
+</html>
