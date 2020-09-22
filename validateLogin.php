9b3180a2cf63bf46e19af46965529063ba25b0da
Khaled Nached <khalednached@Khaleds-MacBook-Pro.local>
2020-09-22 15:26:28 -0400

    made changes to this file

--- validateLogin.php
+++ validateLogin.php
@@ -1,69 +1,63 @@
 <?php
+
 if(isset($_POST['login'])){
   require('dbHandler.php');
-
-  // Grab POST DATA
   $email = $_POST['email'];
   $passWord = $_POST['passWord'];
-  if(empty($email)||empty($passWord)){
+
+// check if the fields are empty
+  if(empty($email) || empty($passWord)){
     header('Location: login.php?error=emptyfields');
     exit();
   }
-  // validate the email
-  else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    header('Location: login.php?error=invalidemail');
-    exit();
-  }
-  // validate the password is the correct length
-  else if(strlen(string)$passWord <8)
-  {
-    header('Location: login.php?error=invalidpassword&email='.$email.'');
-    exit();
-  }
   else{
-    // if everything is all good
-    $connectToDb = mysqli_connect($serverName, $user, $password, $dbname);
-    if(!$connectToDb){
-      die("Connection failed".mysqli_connect_errno().mysqli_connect_error());
-    }
-    $query = 'SELECT email, passWord FROM newUsers WHERE email=? AND passWord=?';
+    $query = 'SELECT * FROM newUsers WHERE email=?';
     // initialize the prepared statement
-    $stmtPrepared = mysqli_stmt_init($connectToDb);
-    if(!mysqli_stmt_prepare($stmtPrepared, $query)){
-      header('Location: login.php?error=sqlError')
+    $preparedStmt = mysqli_stmt_init($connectToDb);
+
+    // run the SQL and prepared statement
+    // runs query string in the DB and checks for errors
+    if(!mysqli_stmt_prepare($preparedStmt, $query)){
+      header('Location: login.php?error=sqlerror');
       exit();
-    }
-    // If the Prepared statement was successful
-    // bind the parameters
-    mysqli_stmt_bind_param($stmtPrepared, 'ss', $email, $passWord);
-    // execute the prepared statement
-    mysqli_stmt_execute($stmtPrepared);
-    // store the results in the prepared statment variable
-    $results = mysqli_get_result($stmtPrepared);
-    if($row = mysqli_fetch_assoc($results))
-    {
-      $checkPassword = password_verify($passWord, $row['passWord']);
-      if($checkPassword == FALSE){
-        header('Location: login.php?error=wrongpassword&email='.$email.'');
-        exit();
-      }
-      else if($checkPassword == TRUE){
-        // if pwd is matched against the one in the DB, then start the session
-        session_start();
-        $_SESSION['passWord']=$row['passWord'];
-        $_SESSION['email']=$row['email'];
-        // redirect the user to their dashboard
-        header('Location: userDashboard.php?success=userloggedin');
-        exit();
-      }
-      else{
-        header('Location: login.php?error=wrongpassword&email='.$email.'');
-        exit();
-      }
     }
     else{
-      header('Location: login.php?error=nouser');
-      exit();
+      // if no errors in the SQL query
+      mysqli_stmt_bind_param($preparedStmt, "s", $email);
+      // now execute the binded prepared statement
+      mysqli_stmt_execute($preparedStmt);
+
+      // get results from the DB
+      $results = mysqli_stmt_get_result($preparedStmt);
+      // check to see if we got results from the DB
+
+      // fetch data from the result and place results in an ASSOCIATIVE ARRAY
+      // which is the format that can be worked with in PHP
+      if($row = mysqli_fetch_assoc($results)){
+        // grab the password from the user, hash it and see if it matches with the pwd that the user tried to login with
+        $checkPwd = password_verify($passWord, $row['passWord']);
+        if($checkPwd == false){
+          header('Location: login.php?error=wrongpassword&email='.$email.'');
+          exit();
+        }
+        else if($checkPwd == true){
+          session_start(); // start a session to remember the user on the SERVER
+          $_SESSION['email']=$row['email'];
+          header('Location: userDashboard.php?success=userLoggedin');
+          exit();
+        }
+        else{
+          // if the user entered a password that is not a string
+          header('Location: login.php?error=wrongpassword&email='.$email.'');
+          exit();
+        }
+      }
+      else{
+        // if no results were matched in the DB
+        header('Location: login.php?error=nouser');
+        exit();
+      }
+
     }
   }
 }
@@ -71,4 +65,6 @@ else{
   header('Location: homepage.php');
   exit();
 }
+
+
 ?>
